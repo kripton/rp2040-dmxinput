90c4dd1cf40d7ee2859a434f581c3e509e7dbb01
Jannis Achstetter <kripton@kripserver.net>
2023-03-05 23:59:35 +0100

    Enable stdio output via USB and fixups

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,23 @@ extern "C" {
 
 #include "pins.h"
 #include "picotool_binary_information.h"
+
+#include "DmxInput.h"
 }
 
 extern "C" {
+    uint8_t DMXBuffer[8][513]; // 8 universes, start code + 512 channels
+}
 
+DmxInput in_0;   // The instance of DmxInput for universe 0
+DmxInput in_1;   // The instance of DmxInput for universe 1
+DmxInput in_2;   // The instance of DmxInput for universe 2
+DmxInput in_3;   // The instance of DmxInput for universe 3
+
+void __isr input_updated_c(DmxInput* instance) {
+    printf("DMX input received\n");
+    // Toggle the LED
+    gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
 }
 
 int main() {
@@ -23,7 +36,14 @@ int main() {
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
+    stdio_init_all();
+
+    // Set up a DMX input
+    DmxInput::return_code retVal = in_0.begin(PIN_DMXIN_0, 0, 512, pio1, false);
+    in_0.read_async(DMXBuffer[0], input_updated_c);
+
     while (true) {
-        sleep_us(10);
+        sleep_ms(1000);
+        printf("ping\n");
     }
 };
